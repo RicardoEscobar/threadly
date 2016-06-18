bd93301ba166e275fe696eb9d7f2555132f701e5
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2016-06-18 10:52:47 -0500

    In script.js, inside the submit event handler above the if statement, fetch the value typed into the <input id=comment> using .val(). Save the value into a variable named comment

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ SOFTWARE.
 */
 var main = function() {
   $('form').submit(function() {
-
+    var comment = $("#comment").val();
     if(comment !== "") {
       var html = $('<li>').text(comment);
 
